761729f911f6740b717e52769278b8c5a702f41d
Guilherme Souza <guilhermespublic@gmail.com>
2018-10-29 10:55:48 -0300

    Primeiro controller e página de testes Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -14,7 +14,8 @@ namespace SalesWebMvc.Controllers {
 
         public IActionResult About() {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["DonoSite"] = "Guilherme Souza";
+        
             return View();
         }
 
